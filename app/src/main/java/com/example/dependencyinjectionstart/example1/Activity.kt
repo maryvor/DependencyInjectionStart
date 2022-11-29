406cd0828ee59a132ec92f81b7a0680eed7ca36b
MariaVorobieva <74699386+Maria-ru@users.noreply.github.com>
2022-11-29 21:04:11 +0300

    inject constructors

--- app/src/main/java/com/example/dependencyinjectionstart/example1/Activity.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example1/Activity.kt
@@ -1,14 +1,26 @@
 package com.example.dependencyinjectionstart.example1
 
-class Activity {
+import javax.inject.Inject
 
-    val monitor = Monitor()
-    val keyboard = Keyboard()
-    val mouse = Mouse()
-    val computerTower = ComputerTower(
-        Storage(),
-        Memory(),
-        Processor()
-    )
-    val computer = Computer(monitor, computerTower, keyboard, mouse)
+class Activity {
+    //First method
+//    @Inject
+//    lateinit var computer: Computer
+//    @Inject
+//    lateinit var keyboard: Keyboard
+//    @Inject
+//    lateinit var mouse: Mouse
+//    @Inject
+//    lateinit var monitor: Monitor
+//
+//    init {
+//        DaggerNewComponent.create().inject(this)
+//    }
+
+    //Second method
+
+    val keyboard = DaggerNewComponent.create().getKeyboard()
+    val mouse = DaggerNewComponent.create().getMouse()
+    val monitor = DaggerNewComponent.create().getMonitor()
+    val computer = DaggerNewComponent.create().getComputer()
 }
---
+++ app/src/main/java/com/example/dependencyinjectionstart/example1/ComputerModule.kt
@@ -0,0 +1,10 @@
+package com.example.dependencyinjectionstart.example1
+
+import dagger.Module
+import dagger.Provides
+//
+//@Module
+//class ComputerModule {
+//    @Provides
+//    fun provideMo
+//}
