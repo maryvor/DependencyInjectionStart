dd5a1c9abf03bef7a110afd61d34b98b3ea08bce
MariaVorobieva <74699386+Maria-ru@users.noreply.github.com>
2022-12-02 14:39:50 +0300

    Add Singleton

--- app/src/main/java/com/example/dependencyinjectionstart/example2/presentation/ExampleApplication.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example2/presentation/ExampleApplication.kt
@@ -1,6 +1,17 @@
 package com.example.dependencyinjectionstart.example2.presentation
 
 import android.app.Application
+import com.example.dependencyinjectionstart.example2.di.DaggerAppComponent
 
 class ExampleApplication: Application() {
+
+    val component by lazy {
+        //with @Component.Builder
+//        DaggerAppComponent.builder()
+//            .context(application)
+//            .timeMillis(Calendar.getInstance().timeInMillis)
+//            .build()
+
+        DaggerAppComponent.factory().create(this, System.currentTimeMillis())
+    }
 }
--- app/src/main/java/com/example/dependencyinjectionstart/example2/presentation/MainActivity.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example2/presentation/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.dependencyinjectionstart.R
 import com.example.dependencyinjectionstart.example1.Activity
-import com.example.dependencyinjectionstart.example2.di.DaggerAppComponent
 import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
@@ -12,13 +11,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var viewModel: ExampleViewModel
 
     private val component by lazy {
-        //with @Component.Builder
-//        DaggerAppComponent.builder()
-//            .context(application)
-//            .timeMillis(Calendar.getInstance().timeInMillis)
-//            .build()
-
-        DaggerAppComponent.factory().create(application, System.currentTimeMillis())
+        (application as ExampleApplication).component
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
